b7d3816d6269de418726e087fd84cd92509f4fe5
mjh2550 <moomjh94@naver.com>
2022-10-07 00:11:22 +0900

    Firebase realtime database setting

--- app/src/main/java/com/android/dailynote/ui/viewmodel/NoteListViewModel.kt
+++ app/src/main/java/com/android/dailynote/ui/viewmodel/NoteListViewModel.kt
@@ -2,11 +2,24 @@ package com.android.dailynote.ui.viewmodel
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.android.dailynote.BuildConfig
+import com.android.dailynote.R
 import com.android.dailynote.base.BaseViewModel
 import com.android.dailynote.data.model.entity.NoteVO
+import com.android.dailynote.data.model.firebase.FireBaseDataSource
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import java.time.LocalDateTime
 
 class NoteListViewModel : BaseViewModel(){
+
+    companion object{
+        private const val FIREBASE_DB_URL = BuildConfig.FIREBASE_URL
+        private val database = Firebase.database(FIREBASE_DB_URL)
+        private val myRef = database.getReference("daily_note")
+    }
+
     //TODO TEST LIST
     val dateAndtime: LocalDateTime = LocalDateTime.now()
     val testVO = NoteVO(
@@ -50,4 +63,10 @@ class NoteListViewModel : BaseViewModel(){
     )
     val list = arrayListOf(testVO,testVO2,testVO3)
     val dataList = MutableLiveData<ArrayList<NoteVO>>(list)
+    var cnt = 1
+    fun click() {
+        myRef.setValue("hello World ${cnt++}")
+        println("OK")
+    }
+
 }
---
+++ data/src/main/java/com/android/dailynote/data/model/firebase/FireBaseDataSource.kt
@@ -0,0 +1,7 @@
+package com.android.dailynote.data.model.firebase
+
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+class FireBaseDataSource {
+}
