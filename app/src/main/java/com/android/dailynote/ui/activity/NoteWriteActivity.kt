9c50c2a93471248e525bab429498914c0bfa2c67
jhmoon <moomjh94@naver.com>
2023-04-19 16:36:26 +0900

    다이얼로그 메세지, 제목란 비울 시 날짜 자동세팅되도록 수정

--- app/src/main/java/com/android/dailynote/ui/activity/NoteWriteActivity.kt
+++ app/src/main/java/com/android/dailynote/ui/activity/NoteWriteActivity.kt
@@ -5,14 +5,17 @@ import android.content.DialogInterface
 import android.content.Intent
 import android.view.View
 import android.view.View.OnClickListener
+import android.widget.EditText
 import android.widget.Toast
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.android.dailynote.R
 import com.android.dailynote.base.BaseActivity
+import com.android.dailynote.common.TimeClass
 import com.android.dailynote.data.model.roomdb.NoteRepository
 import com.android.dailynote.databinding.ActivityNoteWriteBinding
 import com.android.dailynote.ui.viewmodel.NoteWriteViewModel
+import java.util.*
 
 class NoteWriteActivity : BaseActivity<ActivityNoteWriteBinding,NoteWriteViewModel>() ,OnClickListener {
     override val mViewModel: NoteWriteViewModel by lazy {
@@ -82,12 +85,12 @@ class NoteWriteActivity : BaseActivity<ActivityNoteWriteBinding,NoteWriteViewMod
 
     private fun isValidateData() : Boolean{
         if(mDataBinding.edtNoteTitle.text.isEmpty()){
-            //Error alert
-            Toast.makeText(this@NoteWriteActivity,"글 제목이 비어있습니다.",Toast.LENGTH_SHORT).show()
-            return false
+            val now = TimeClass().dateToString(Date(),"YYYY-MM-dd")
+            val arr = now.split("-")
+            mDataBinding.edtNoteTitle.setText("${arr[0]}년 ${arr[1]}월 ${arr[2]}일의 일기")
         }else if(mDataBinding.edtNoteContents.text.isEmpty()){
             //Error alert
-            Toast.makeText(this@NoteWriteActivity,"내용이 비어있습니다.",Toast.LENGTH_SHORT).show()
+            Toast.makeText(this@NoteWriteActivity,"내용이 비어있습니다.", Toast.LENGTH_SHORT).show()
             return false
         }
 
@@ -98,13 +101,15 @@ class NoteWriteActivity : BaseActivity<ActivityNoteWriteBinding,NoteWriteViewMod
         //TODO 디자인 커스텀 필요
         if(isSave) {
             val builder = AlertDialog.Builder(this)
-                .setTitle("입력한 데이터를 저장하시겠습니까?")
+                .setTitle("알림")
+                .setMessage("입력한 데이터를 저장하시겠습니까?")
                 .setPositiveButton("확인") { dialog, which -> onClickSave() }
                 .setNegativeButton("취소") { dialog, which -> dialog.dismiss() }
                 .show()
         } else {
             val builder = AlertDialog.Builder(this)
-                .setTitle("입력하던 데이터를 초기화하고 메인으로 이동하시겠습니까?\n초기화한 데이터는 저장되지 않습니다.")
+                .setTitle("알림")
+                .setMessage("입력하던 데이터를 초기화하고 메인으로 이동하시겠습니까?\n초기화한 데이터는 저장되지 않습니다.")
                 .setPositiveButton("확인") { dialog, which -> onClickCancel() }
                 .setNegativeButton("취소") { dialog, which -> dialog.dismiss() }
                 .show()
--- app/src/main/java/com/android/dailynote/ui/viewmodel/NoteWriteViewModel.kt
+++ app/src/main/java/com/android/dailynote/ui/viewmodel/NoteWriteViewModel.kt
@@ -6,6 +6,7 @@ import androidx.activity.result.contract.ActivityResultContract
 import androidx.core.app.ActivityOptionsCompat
 import androidx.lifecycle.MutableLiveData
 import com.android.dailynote.base.BaseViewModel
+import com.android.dailynote.common.TimeClass
 import com.android.dailynote.data.model.entity.NoteVO
 import com.android.dailynote.data.model.roomdb.NoteRepository
 import com.android.dailynote.ui.activity.HomeActivity
@@ -16,4 +17,6 @@ class NoteWriteViewModel(private val repository: NoteRepository) : BaseViewModel
 //    val isDataOnLoad = MutableLiveData(false)
     val edtContentsText = MutableLiveData("")
     val edtTitleText = MutableLiveData("")
+    private val now = TimeClass().dateToString(Date(), "YYYY-MM-dd").split("-")
+    val dateStr = "${now[0]}년 ${now[1]}월 ${now[2]}일의 일기"
 }
